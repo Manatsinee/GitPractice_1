c8c0829f3595388bc2c1eebba45a360512d334e0
Manatsinee <littlebring@gmail.com>
2021-04-24 16:53:32 -0400

    Added more work after publishing to GitHub

--- src/day1_git/HelloWorld.java
+++ src/day1_git/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Adding GitHub now");
+        System.out.println("YAY!!");
 
     }
 }
